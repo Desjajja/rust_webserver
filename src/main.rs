ce8121b7efa902ab42dc9d428197e5989ebd2608
Desjajja <desjajja@gmail.com>
2023-10-27 16:38:20 +0800

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
 use std::io::{BufReader, BufRead, Write};
-
+use regex::Regex;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,12 +16,19 @@ fn main() {
             Ok(mut stream) => {
                 let buf_reader = BufReader::new(&stream);
                 let header = buf_reader.lines().next().unwrap().unwrap();
-                let status_line = match &header[..] {
-                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
-                    _ => "HTTP/1.1 404 Not Found\r\n\r\n"
+                let re = Regex::new(r"\/\w+\s").unwrap();
+                let response = match re.captures(&header) {
+                    Some(caps) => {
+                        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+                        let content_type = "Content-Type: text/plain\r\n\r\n";
+                        let content = &caps[0][1..].trim();
+                        let length = content.len();
+                        format!("{status_line}{content_type}{length}{content}\r\n\r\n")
+                    }
+                    _ => String::from("HTTP/1.1 404 Not Found\r\n\r\n")
                 };
                 
-                stream.write_all(status_line.as_bytes()).unwrap();
+                stream.write_all(response.as_bytes()).unwrap();
                 println!("responded to connection");
             }
             Err(e) => {
