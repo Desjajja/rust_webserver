c661db9ead20c11cb25ac969892caca47959cf75
Desjajja <desjajja@gmail.com>
2023-10-27 15:03:00 +0800

    pass 3nd stage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::net::TcpListener;
 use std::io::{BufReader, BufRead, Write};
 
+use nom::AsBytes;
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -14,10 +16,13 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 let buf_reader = BufReader::new(&stream);
-                let _header = buf_reader.lines().next().unwrap().unwrap();
-
-                let status_line = b"HTTP/1.1 200 OK\r\n\r\n";
-                stream.write_all(status_line).unwrap();
+                let header = buf_reader.lines().next().unwrap().unwrap();
+                let status_line = match &header[..] {
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n"
+                };
+                
+                stream.write_all(status_line.as_bytes()).unwrap();
                 println!("responded to connection");
             }
             Err(e) => {
