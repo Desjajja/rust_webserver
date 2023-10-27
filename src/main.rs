c0a3b2503b2066e2a7517efdb9d99f0a10935b75
Desjajja <desjajja@gmail.com>
2023-10-27 17:06:22 +0800

    pass 4th stage, fix response

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,23 @@ fn main() {
             Ok(mut stream) => {
                 let buf_reader = BufReader::new(&stream);
                 let header = buf_reader.lines().next().unwrap().unwrap();
-                let re = Regex::new(r"\/\w+\s").unwrap();
+                let re = Regex::new(r"\/\w*\s").unwrap();
                 let response = match re.captures(&header) {
                     Some(caps) => {
                         let status_line = "HTTP/1.1 200 OK";
                         let content_type = "text/plain";
                         let content = &caps[0][1..].trim();
                         let length = content.len();
-                        format!(
-                            r"{status_line}\r\n\r\nContent-Type: {content_type}\r\n\r\nContent-Length: {length}\r\n\r\n{content}\r\n\r\n"
-                        )
+                        let response: String;
+                        if length == 0 {
+                            println!("length of content is 0");
+                            response = format!(r"{status_line}\r\n\r\n}");
+                        } else {
+                            response = format!(
+                                r"{status_line}\r\n\r\nContent-Type: {content_type}\r\n\r\nContent-Length: {length}\r\n\r\n{content}\r\n\r\n"
+                            );
+                        }
+                        response
                     }
                     _ => String::from("HTTP/1.1 404 Not Found\r\n\r\n"),
                 };
