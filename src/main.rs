aa5f6dcd93bc6e48ae2f711e17e2fcd17fd9d388
Desjajja <desjajja@gmail.com>
2023-10-27 17:04:09 +0800

    pass 4th stage, fix response

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
             Ok(mut stream) => {
                 let buf_reader = BufReader::new(&stream);
                 let header = buf_reader.lines().next().unwrap().unwrap();
-                let re = Regex::new(r"\/\w*\s").unwrap();
+                let re = Regex::new(r"\/\w+\s").unwrap();
                 let response = match re.captures(&header) {
                     Some(caps) => {
                         let status_line = "HTTP/1.1 200 OK";
