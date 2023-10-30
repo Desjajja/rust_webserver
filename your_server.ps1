16caf33c4294f3888a9a96c1a9cf3eaf58cf70f9
Desjajja <desjajja@gmail.com>
2023-10-30 17:00:41 +0800

    7th commit: get a file

--- your_server.ps1
+++
@@ -1,7 +0,0 @@
-# This is a PowerShell script to execute a Rust Cargo command
-# Replace 'YourCargoManifestPath' with the actual path to your Cargo.toml file
-
-$CargoManifestPath = Join-Path (Get-Item $PSScriptRoot).FullName "YourCargoManifestPath"
-$TargetDir = "C:\tmp\codecrafters-http-server-target" # Replace with your desired target directory
-
-& cargo run --quiet --release --target-dir=$TargetDir --manifest-path $CargoManifestPath -- $args
