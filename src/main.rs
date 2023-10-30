5d76a87ac3956e754f722778c26f231eb54b93b4
Desjajja <desjajja@gmail.com>
2023-10-30 13:53:47 +0800

    6th: concurrency

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     // Uncomment this block to pass the first stage
     
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    let pool = ThreadPool::new(7);
+    let pool = ThreadPool::new(4);
     for _stream in listener.incoming() {
         let stream = _stream.unwrap();
         pool.execute(||{
