dc1468786aef90629d1123bd3f6536022a575870
Desjajja <desjajja@gmail.com>
2023-10-30 13:41:21 +0800

    6th: concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // Uncomment this block to pass the first stage
 use std::io::{BufRead, BufReader, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+// use anyhow::{Result};
 use http_server_starter_rust::response::{get_response, Status, ContentType};
+use threadpool::ThreadPool;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,41 +12,42 @@ fn main() {
     // Uncomment this block to pass the first stage
     
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let buf_reader = BufReader::new(&stream);
-                let mut request = buf_reader.lines();
-                let header = request.nth(0).unwrap().unwrap();
-                let uri = header.split(' ').nth(1).unwrap();
-                // let host: String = request.nth(2).unwrap().unwrap();
-                let mut user_agent = request.filter(|line| {line.as_ref().unwrap().starts_with("User-Agent")});
-                let (status, content_type, content) = match uri {
-                    "/" => (Status::OK, ContentType::Unknown, None),
-                    _ => {
-                        if uri.starts_with("/echo") {
-                            let content = uri.split_once("/echo/").unwrap().1.to_owned();
-                            (Status::OK, ContentType::TextPlain, Some(content))
-                        } else if uri.starts_with("/user-agent"){
-                            let user_agent = user_agent.next().unwrap().unwrap();
-                            let content = user_agent.split_once(' ').unwrap().1.to_owned();
-                            println!("matched field:{}", user_agent.split_once(' ').unwrap().0);
-                            (Status::OK, ContentType::TextPlain, Some(content))
-                        }
-                        else
-                        {
-                            (Status::NotFound, ContentType::Unknown, None)
-                        }
-                    }
-                };
-                let response = get_response(status, content_type, content);
-                stream.write_all(response.as_bytes()).unwrap();
-                println!("responded to connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    let pool = ThreadPool::new(7);
+    for _stream in listener.incoming() {
+        let stream = _stream.unwrap();
+        pool.execute(||{
+            handle_stream(stream);
+        });
     }
 }
+
+fn handle_stream(mut stream: TcpStream) {
+
+            let buf_reader = BufReader::new(&stream);
+            let mut request = buf_reader.lines();
+            let header = request.nth(0).unwrap().unwrap();
+            let uri = header.split(' ').nth(1).unwrap();
+            // let host: String = request.nth(2).unwrap().unwrap();
+            let user_agent = request.find(|line| {line.as_ref().unwrap().starts_with("User-Agent")});
+            let (status, content_type, content) = match uri {
+                "/" => (Status::OK, ContentType::Unknown, None),
+                _ => {
+                    if uri.starts_with("/echo") {
+                        let content = uri.split_once("/echo/").unwrap().1.to_owned();
+                        (Status::OK, ContentType::TextPlain, Some(content))
+                    } else if uri.starts_with("/user-agent"){
+                        let user_agent = user_agent.unwrap().unwrap();
+                        let content = user_agent.split_once(' ').unwrap().1.to_owned();
+                        (Status::OK, ContentType::TextPlain, Some(content))
+                    }
+                    else
+                    {
+                        (Status::NotFound, ContentType::Unknown, None)
+                    }
+                }
+            };
+            let response = get_response(status, content_type, content);
+            stream.write_all(response.as_bytes()).unwrap();
+            println!("responded to connection");
+
+    }
