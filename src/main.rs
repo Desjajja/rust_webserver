2ae5f59e4806f05d6286fef7eff2193bb1c167d4
Desjajja <desjajja@gmail.com>
2023-11-03 11:26:32 +0800

    migrate to tokio, respond function refactored

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
-use http_server_starter_rust::response::{get_response, Status, ContentType};
+use tokio::fs::File;
+use http_server_starter_rust::response::*;
 use http_server_starter_rust::request::{HttpRequest, parse_request};
 use std::path::PathBuf;
 
@@ -37,99 +38,59 @@ async fn process_request(socket: &mut TcpStream, root_dir: Option<String>) -> Re
     let response = match method {
         method if method == "post" => {
             match uri {
-                // uri if uri.starts_with("/files") => post_file(request, root_dir),
-                _ => handle_404(),
+                uri if uri.starts_with("/files") => post_file(request, root_dir).await,
+                _ => respond_405(),
             }
         },
         method if method =="get" => {
             match uri {
-                uri if uri == "/" => handle_ok(),
+                uri if uri == "/" => respond_200(),
                 uri if uri.starts_with("/echo") => handle_echo(request),
                 uri if uri.starts_with("/user-agent") => handle_user_agent(request),
-                // uri if uri.starts_with("/files") => get_files(request, root_dir),
-                _ => handle_404(),
+                uri if uri.starts_with("/files") => get_files(request, root_dir).await,
+                _ => respond_404(),
             }
         },
-        _ => handle_404(),
+        _ => respond_404(),
     };
     socket.write_all(response.as_bytes()).await?;
-    socket.shutdown().await?;
-
     Ok(())
 }
 
-// fn handle_connection(mut stream: TcpStream, root_dir: Option<String> ) {
-//             let mut buffer = [0u8; 2048] ; // 2KB sized buffer for header
-//             stream.read(& mut buffer).unwrap();
-//             let request = String::from_utf8_lossy(& buffer);
-
-//             if let Some(parsed_req) = parse_request(&request) {
-//                 let method = parsed_req.method.unwrap().to_lowercase();
-//                 let uri = parsed_req.path.unwrap();
-                // let response = match method {
-                //     method if method == "post" => {
-                //         match uri {
-                //             uri if uri.starts_with("/files") => post_file(parsed_req, root_dir),
-                //             _ => handle_404(),
-                //         }
-                //     },
-                //     method if method =="get" => {
-                //         match uri {
-                //             uri if uri == "/" => handle_ok(),
-                //             uri if uri.starts_with("/echo") => handle_echo(parsed_req),
-                //             uri if uri.starts_with("/user-agent") => handle_user_agent(parsed_req),
-                //             uri if uri.starts_with("/files") => get_files(parsed_req, root_dir),
-                //             _ => handle_404(),
-                //         }
-                //     },
-                //     _ => handle_404(),
-                // };
-//                 stream.write_all(response.as_bytes()).unwrap();  
-//             }
-//                       }
-
-// fn post_file(req: HttpRequest, root_dir: Option<String>) -> String {
-//     let filename = req.path.unwrap().split('/').last().unwrap().to_owned();
-//     let mut path_buffer = PathBuf::new();
-//     path_buffer.push(PathBuf::from(root_dir.unwrap_or_default()));
-//     path_buffer.push(PathBuf::from(filename));
-//     match std::fs::File::create(path_buffer) {
-//         Ok(mut f) => f.write_all(req.body.unwrap().as_bytes()).unwrap(),
-//         Err(_) => return handle_404(),
-//     }
-//     handle_201()
-// }
-
-// fn handle_201() -> String {
-//     get_response(Status::Created, ContentType::None, None)
-// }
+async fn post_file(req: HttpRequest<'_>, root_dir: Option<String>) -> String {
+    let filename = req.path.unwrap().split('/').last().unwrap().to_owned();
+    let mut path_buffer = PathBuf::new();
+    path_buffer.push(PathBuf::from(root_dir.unwrap_or_default()));
+    path_buffer.push(PathBuf::from(filename));
+    match File::create(path_buffer).await {
+        Ok(mut f) => f.write_all(req.body.unwrap().as_bytes()).await.unwrap(),
+        Err(_) => return respond_404(),
+    }
+    respond_201()
+}
 
 fn handle_echo(req: HttpRequest) -> String {
     let content = req.path.unwrap().split_once("/echo/").unwrap().1.to_owned();
-    get_response(Status::OK, ContentType::TextPlain, Some(content))
+    respond_200_with_text(content)
 }
 
-fn handle_ok() -> String {
-    get_response(Status::OK, ContentType::Unknown, None)
-}
 
 fn handle_user_agent(req: HttpRequest) -> String{
     let content = req.header_fields.get("user-agent").unwrap().to_owned().to_owned();
-    get_response(Status::OK, ContentType::TextPlain, Some(content))
+    respond_200_with_text(content)
 }
 
-// fn get_files(req: HttpRequest, root_dir: Option<String>) -> String {
-//     let filename = req.path.unwrap().split('/').last().unwrap().to_owned();
-//     let mut path_buffer = PathBuf::new();
-//     path_buffer.push(PathBuf::from(root_dir.unwrap_or_default()));
-//     path_buffer.push(PathBuf::from(filename));
-//     if !path_buffer.exists() {
-//         return handle_404();
-//     }   
-//     let content = std::fs::read_to_string(path_buffer).unwrap();
-//     get_response(Status::OK, ContentType::File, Some(content))
-// }
+async fn get_files(req: HttpRequest<'_>, root_dir: Option<String>) -> String {
+    let filename = req.path.unwrap().split('/').last().unwrap().to_owned();
+    let mut path_buffer = PathBuf::new();
+    path_buffer.push(PathBuf::from(root_dir.unwrap_or_default()));
+    path_buffer.push(PathBuf::from(filename));
+    if !path_buffer.exists() {
+        return respond_404();
+    }
 
-fn handle_404() -> String {
-    get_response(Status::NotFound, ContentType::Unknown, None)
+    let mut buffer = String::new();
+    let mut f =  File::open(path_buffer).await.unwrap();
+    f.read_to_string(&mut buffer).await.unwrap();
+    respond_200_with_file(buffer)
 }
--- src/response.rs
+++ src/response.rs
@@ -1,4 +1,30 @@
-pub fn get_response(status: Status, content_type: ContentType, content: Option<String>) -> String {
+pub fn respond_201() -> String {
+    get_response(Status::Created, ContentType::None, None)
+}
+
+pub fn respond_200() -> String {
+    get_response(Status::OK, ContentType::Unknown, None)
+}
+
+pub fn respond_200_with_text(content: String) -> String {
+	get_response(Status::OK, ContentType::TextPlain, Some(content))
+}
+
+pub fn respond_200_with_file(content: String) -> String {
+	get_response(Status::OK, ContentType::File, Some(content))
+}
+
+
+
+pub fn respond_404() -> String {
+    get_response(Status::NotFound, ContentType::Unknown, None)
+}
+
+pub fn respond_405() -> String {
+	get_response(Status::NotAllowed, ContentType::None , None)
+}
+
+fn get_response(status: Status, content_type: ContentType, content: Option<String>) -> String {
 	match status {
 		Status::NotFound => "HTTP/1.1 404 Not Found\r\n\r\n".to_owned(),
 		Status::OK => {
@@ -13,16 +39,18 @@ pub fn get_response(status: Status, content_type: ContentType, content: Option<S
 				format!("HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}", content_type, content.len(), content)
 			}
 		},
-		Status::Created => "HTTP/1.1 201 Created\r\n\r\n".to_owned()
+		Status::Created => "HTTP/1.1 201 Created\r\n\r\n".to_owned(),
+		Status::NotAllowed => "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_owned(),
 	}
 }
-pub enum Status {
+enum Status {
 	OK,
 	Created,
 	NotFound,
+	NotAllowed,
 }
 
-pub enum ContentType{
+enum ContentType{
 	TextPlain,
 	File,
 	Unknown,
